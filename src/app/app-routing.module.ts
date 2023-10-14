1e74d5c56becf2f4b14963ab13d2d246124af465
Wellington Fujiwara <w.fujiwara93@gmail.com>
2023-10-13 22:05:57 -0300

    finalizado o style das pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { SobreComponent } from './pages/sobre/sobre.component';
+import { HabilidadesComponent } from './pages/habilidades/habilidades.component';
+import { ProjetosComponent } from './pages/projetos/projetos.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'sobre',
+    component: SobreComponent
+  },
+  {
+    path: 'habilidades',
+    component: HabilidadesComponent
+  },
+  {
+    path: 'projetos',
+    component: ProjetosComponent
   }
 ];
 
